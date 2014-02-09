82bb32bdb60d62df9032845cd54dc0b4ca3cdf2e
Chris Montes <chrismontes@about.me>
2014-02-09 15:25:07 -0800

    Base with postgres and heroku settings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spatula::Application.config.secret_key_base = 'd4d33bbd5154cf98ed302037f00ce183912364a88edacc4264933d9e05b7a1be599a0dafad75d9ed15ca806d5a9610e618219570621f2bd0ec3fe8aa2a29ba0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spatula::Application.config.session_store :cookie_store, key: '_spatula_session'
