fea7c1a26f2f72dc5168b2b501c2b267f2ab1d81
Chris Montes <chrismontes@about.me>
2014-03-08 11:46:58 -0800

    FORM

--- test/helpers/filters_helper.rb
+++ test/helpers/filters_helper.rb
@@ -5,12 +5,12 @@ def fill_in_filter_form(params = {})
     max_price: 1000,
     ending_time: 1,
     ending_time_unit: 'Day',
-    sort_by: 'date-desc'
+    sort_by: 'Ending Soonest'
   }.merge(params)
 
   page.find('#filter-form').fill_in "Search Term", with: params[:search_term]
   page.find('#filter-form').fill_in "Max Price", with: params[:max_price]
   page.find('#filter-form').fill_in "Ending Time", with: params[:ending_time]
   page.find('#filter-form').select params[:ending_time_unit], from: "filter_ending_time_unit"
-  page.find('#filter-form').select params[:ending_time_unit], from: "filter_sort_by"
+  page.find('#filter-form').select params[:sort_by], from: "filter_sort_by"
 end
